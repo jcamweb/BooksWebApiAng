c81c55b74084d974687376c10f7df05fc8f2b8f1
jcarmartin <jcam@jcamweb.com>
2021-03-17 16:38:57 +0100

    Create Database and data in SeedData.

--- SeedData.cs
+++ SeedData.cs
@@ -14,6 +14,17 @@ namespace BooksWebApiAng
         {
             var context = services.GetRequiredService<BookContext>();
            await context.Database.MigrateAsync();
+            //bool created = await context.Database.EnsureCreatedAsync();
+            //string creationinfo = created ? "created" : "exists";
+            //if (created)
+            //{
+            //    var b1 = new Book { Titulo = "Contact", Autor = "Carl Sagan", Editorial = "Nasa" };
+            //    var b2 = new Book { Titulo = "Título2", Autor = "Autor2", Editorial = "Editorial2" };
+            //    var b3 = new Book { Titulo = "Título3", Autor = "Autor3", Editorial = "Editorial3" };
+            //    var b4 = new Book { Titulo = "Título4", Autor = "Autor4", Editorial = "Editorial4" };
+            //    await context.Books.AddRangeAsync(b1, b2, b3, b4);
+            //    int records = await context.SaveChangesAsync();
+            //}
             // To Add Data from Seed
             //if (!await context.Books.AnyAsync())
             //{
@@ -22,5 +33,6 @@ namespace BooksWebApiAng
             //}
         }
 
+       
     }
 }
