7a0853009231ca272711e003acaaa2f1929556ed
jcarmartin <jcam@jcamweb.com>
2021-03-18 08:42:03 +0100

    Updated seedData to add all data to test.

--- SeedData.cs
+++ SeedData.cs
@@ -13,18 +13,19 @@ namespace BooksWebApiAng
            IServiceProvider services)
         {
             var context = services.GetRequiredService<BookContext>();
-           await context.Database.MigrateAsync();
-            //bool created = await context.Database.EnsureCreatedAsync();
-            //string creationinfo = created ? "created" : "exists";
-            //if (created)
-            //{
-            //    var b1 = new Book { Titulo = "Contact", Autor = "Carl Sagan", Editorial = "Nasa" };
-            //    var b2 = new Book { Titulo = "Título2", Autor = "Autor2", Editorial = "Editorial2" };
-            //    var b3 = new Book { Titulo = "Título3", Autor = "Autor3", Editorial = "Editorial3" };
-            //    var b4 = new Book { Titulo = "Título4", Autor = "Autor4", Editorial = "Editorial4" };
-            //    await context.Books.AddRangeAsync(b1, b2, b3, b4);
-            //    int records = await context.SaveChangesAsync();
-            //}
+         
+            bool created = await context.Database.EnsureCreatedAsync();
+            string creationinfo = created ? "created" : "exists";
+            if (created)
+            {
+                var b1 = new Book { Titulo = "Contact", Autor = "Carl Sagan", Editorial = "Nasa" };
+                var b2 = new Book { Titulo = "Título2", Autor = "Autor2", Editorial = "Editorial2" };
+                var b3 = new Book { Titulo = "Título3", Autor = "Autor3", Editorial = "Editorial3" };
+                var b4 = new Book { Titulo = "Título4", Autor = "Autor4", Editorial = "Editorial4" };
+                await context.Books.AddRangeAsync(b1, b2, b3, b4);
+                int records = await context.SaveChangesAsync();
+            }
+            // await context.Database.MigrateAsync();
             // To Add Data from Seed
             //if (!await context.Books.AnyAsync())
             //{
@@ -33,6 +34,6 @@ namespace BooksWebApiAng
             //}
         }
 
-       
+
     }
 }
